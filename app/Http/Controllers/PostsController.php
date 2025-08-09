8e83a248ad64249b3017ab3f271279adb9a54d3b
Tasneem <tasneemm967@gmail.com>
2025-08-09 15:06:44 +0200

    Update post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Posts\CreatePostRequest;
+use App\Http\Requests\Posts\UpdatePostRequest;
 use Illuminate\Http\Request;
 
 use App\Post;
@@ -39,7 +40,7 @@ class PostsController extends Controller
     public function store(CreatePostRequest $request)
     {
        
-        $image = $request->image->store('posts');
+        $image = $request->image->store('posts', 'public');
 
         Post::create([
             'title' => $request->title,
@@ -71,9 +72,10 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view('posts.create')->with('post', $post);
+
     }
 
     /**
@@ -83,9 +85,26 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        //
+
+        $data = $request->only(['title', 'description', 'content', 'published_at']);
+
+        if($request->hasFile('image')) {
+
+            $image = $request->image->store('posts');
+
+            Storage::delete($post->image);
+
+            $data['image'] = $image;
+        }
+
+        $post->update($data);
+
+        session()->flash('success', 'Post updated successfully.');
+
+        return redirect(route('posts.index'));
+
     }
 
     /**
